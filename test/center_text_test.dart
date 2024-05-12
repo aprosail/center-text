b881ad3b42152100b0a02d8e4c269b3b78ec06b1
James Aprosail <aprosail@outlook.com>
2024-05-12 14:01:57 +0800

    test example placeholder

--- test/center_text_test.dart
+++ test/center_text_test.dart
@@ -1,5 +1,24 @@
+import 'package:center_text/center_text.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('placeholder', () => expect(1 + 1, 2));
+
+  testWidgets('example as placeholder', (t) async {
+    await t.pumpWidget(const MaterialApp(home: Scaffold(body: Example())));
+    expect(find.text('example widget'), findsOneWidget);
+  });
+}
+
+class Example extends StatelessWidget {
+  const Example({
+    super.key,
+    this.child = const CenterText('example widget'),
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => child;
 }
