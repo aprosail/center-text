8d8e5e7e890388807f863571ccc1c924574dc0e0
James Aprosail <aprosail@outlook.com>
2024-05-17 01:01:17 +0800

    format rm unnecessary example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:center_text/center_text.dart';
 import 'package:flutter/material.dart';
-import 'package:inherit/inherit.dart';
 
 void main() {
   runApp(const App());
@@ -10,7 +9,5 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      const CenterText('center text demonstration')
-          .wrapInherit(const CenterTextOptions());
+  Widget build(BuildContext context) => const CenterText('center text');
 }
