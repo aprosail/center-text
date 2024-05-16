a02d8f744bc66a62ba8516f5fa15f0da4b4fc571
James Aprosail <aprosail@outlook.com>
2024-05-16 21:26:58 +0800

    inherit example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:center_text/center_text.dart';
 import 'package:flutter/material.dart';
+import 'package:inherit/inherit.dart';
 
 void main() {
   runApp(const App());
@@ -9,7 +10,7 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const CenterText('app root');
-  }
+  Widget build(BuildContext context) =>
+      const CenterText('center text demonstration')
+          .wrapInherit(const CenterTextOptions());
 }
